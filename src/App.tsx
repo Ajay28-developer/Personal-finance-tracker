f80d0eb5172015e8e8a0bf89deb5528c9994b13a
unknown <you@example.com>
2026-06-02 09:14:11 +0530

    build

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,10 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from './assets/vite.svg'
-import heroImg from './assets/hero.png'
+
 import './App.css'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import 'bootstrap/dist/js/bootstrap.bundle.min.js'
 
 function App() {
-  const [count, setCount] = useState(0)
+
 
   return (
     <>
